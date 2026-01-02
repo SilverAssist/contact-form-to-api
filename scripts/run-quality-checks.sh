1233ec476867454ad2ea23c99647440d7ca86ea4
Miguel Colmenares <me@miguelcolmenares.com>
2026-01-02 11:32:23 -0500

    fix: PHPCS should not fail on warnings
    Add -n flag to PHPCS command to ignore warnings and only fail on errors

--- scripts/run-quality-checks.sh
+++ scripts/run-quality-checks.sh
@@ -198,7 +198,8 @@ check_phpcs() {
         fi
     fi
     
-    if $phpcs_cmd $phpcs_args; then
+    # Use -n to ignore warnings (exit code 0 if no errors, even with warnings)
+    if $phpcs_cmd $phpcs_args -n; then
         print_success "PHPCS check passed"
         return 0
     else
